ebab98a192bdd60b061ae92dd466307edc8c4c6d
orangethewell <orangethewell@gmail.com>
2023-11-06 19:08:52 -0300

    fix(yew-notifications): Update to latest version (0.21.0)

--- src/provider.rs
+++ src/provider.rs
@@ -1,7 +1,7 @@
 use std::marker::PhantomData;
 
 use yew::{
-    classes, function_component, html, use_effect_with_deps, use_reducer_eq, Callback, Children, Classes,
+    classes, function_component, html, use_effect_with, use_reducer_eq, Callback, Children, Classes,
     ContextProvider, Html, Properties,
 };
 
@@ -97,7 +97,8 @@ pub fn notifications_provider<
         sender: Some(notifications.dispatcher()),
     };
 
-    use_effect_with_deps(
+    use_effect_with(
+        (!notifications.is_empty(), notifications.dispatcher()),
         |(is_active, sender)| {
             use gloo::timers::callback::Interval;
 
@@ -112,7 +113,6 @@ pub fn notifications_provider<
 
             move || drop(interval)
         },
-        (!notifications.is_empty(), notifications.dispatcher()),
     );
 
     let ns = notifications.notifications.clone();
